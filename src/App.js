d7885de8fdfedcdc362d2293ade34db18456c595
長谷川 裕大 <gssbtsm999@gmail.com>
2021-05-23 09:38:59 +0900

    U

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import DemoBEM from './components/DemoBEM'
 import DemoCSSModules from './components/DemoCSSModules'
 import DemoStyledComponents from './components/DemoStyledComponents'
-import ReactDOMServer from 'react-dom/server'
 import './App.css'
-
-const CodeBlock = ({ element }) => {
-  return (
-    <pre style={{ width: 300, backgroundColor: 'lightgray', whiteSpace: 'pre-wrap' }}>
-      <code>{ReactDOMServer.renderToStaticMarkup(element)}</code>
-    </pre>
-  )
-}
 
 const App = () => {
   return (
     <div>
       <h1>Demo</h1>
       <DemoBEM />
-      <CodeBlock element={<DemoBEM />} />
       <DemoCSSModules />
       <DemoStyledComponents />
     </div>
